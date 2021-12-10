b24093caa2e937dc1023a5857e037f3385523db7
Edward Castaneda <aeghin7@gmail.com>
2021-12-10 11:15:35 -0600

    react pages

---
+++ client/src/pages/Admin.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Admin.css';
+export default function Admin() {
+    return (
+        <div className='test5'>
+            <h1>Admin page</h1>
+        </div>
+    )
+}
---
+++ client/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Home.css';
+
+
+export default function Home() {
+    return (
+        <div className='test1'>
+            <h1>Homepage</h1>
+            <p>Two buttons will be here( login/sign up?)</p>
+        </div>
+    )
+}
