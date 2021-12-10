3d40c0b1af4cbf66d7293aac44a001641276def1
Danmorales2287 <danmorales2287@gmail.com>
2021-12-10 14:21:14 -0600

    Abilities Info filled into the seeds.js

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -52,13 +52,66 @@ await Ability.deleteMany();
         fullName: 'Strength', 
         desc: ["Strength measures bodily power, athletic training, and the extent to which you can exert raw physical force.",
         "A Strength check can model any attempt to lift, push, pull, or break something, to force your body through a space, or to otherwise apply brute force to a situation. The Athletics skill reflects aptitude in certain kinds of Strength checks."],
-        scoreTotal: 14,
-        modifier: '+3',
-        savingThrow: '-1',
+        scoreTotal: 10,
+        modifier: '+1',
+        savingThrow: '-4',
         skills: ['Athletics']
     },
-
-
+    {
+      character: characters[0]._id,
+      shortName: 'DEX',
+      fullName: 'Dexterity', 
+      desc: ["Dexterity measures agility, reflexes, and balance.",
+      "A Dexterity check can model any attempt to move nimbly, quickly, or quietly, or to keep from falling on tricky footing. The Acrobatics, Sleight of Hand, and Stealth skills reflect aptitude in certain kinds of Dexterity checks."],
+      scoreTotal: 10,
+      modifier: '+1',
+      savingThrow: '-4',
+      skills: ['Acrobatics', 'Sleight of Hand', 'Stealth']
+    },
+    {
+      character: characters[0]._id,
+      shortName: 'CON',
+      fullName: 'Constitution', 
+      desc: ["Constitution measures health, stamina, and vital force.",
+      "Constitution checks are uncommon, and no skills apply to Constitution checks, because the endurance this ability represents is largely passive rather than involving a specific effort on the part of a character or monster."],
+      scoreTotal: 10,
+      modifier: '+1',
+      savingThrow: '-4',
+      skills: []
+  },
+  {
+    character: characters[0]._id,
+    shortName: 'INT',
+    fullName: 'Intelligence', 
+    desc: ["Intelligence measures mental acuity, accuracy of recall, and the ability to reason.",
+    "An Intelligence check comes into play when you need to draw on logic, education, memory, or deductive reasoning. The Arcana, History, Investigation, Nature, and Religion skills reflect aptitude in certain kinds of Intelligence checks."],
+    scoreTotal: 10,
+    modifier: '+1',
+    savingThrow: '-4',
+    skills: ["Arcana", "History", "Investigation", "Nature", "Religion"]
+  },
+  {
+    character: characters[0]._id,
+    shortName: 'WIS',
+    fullName: 'Wisdom', 
+    desc: ["Wisdom reflects how attuned you are to the world around you and represents perceptiveness and intuition.",
+    "A Wisdom check might reflect an effort to read body language, understand someone's feelings, notice things about the environment, or care for an injured person. The Animal Handling, Insight, Medicine, Perception, and Survival skills reflect aptitude in certain kinds of Wisdom checks."],
+    scoreTotal: 10,
+    modifier: '+1',
+    savingThrow: '-4',
+    skills: ["Animal Handling", "Insight", "Medicine", "Perception", "Survival"]
+  },
+  {
+    character: characters[0]._id,
+    shortName: 'CHA',
+    fullName: 'Charisma', 
+    desc: ["Charisma measures your ability to interact effectively with others. It includes such factors as confidence and eloquence, and it can represent a charming or commanding personality.",
+    "A Charisma check might arise when you try to influence or entertain others, when you try to make an impression or tell a convincing lie, or when you are navigating a tricky social situation. The Deception, Intimidation, Performance, and Persuasion skills reflect aptitude in certain kinds of Charisma checks."],
+    scoreTotal: 10,
+    modifier: '+1',
+    savingThrow: '-4',
+    skills: ["Deception", "Intimidation", "Performance", "Persuasion"]
+  },
 ]);
 await Skill.deleteMany();
 
