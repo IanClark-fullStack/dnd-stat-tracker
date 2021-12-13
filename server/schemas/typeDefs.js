4ee1fd1c8b5f82b6f4c29f060f03b64d948c5704
Ian Clark <IanClark2021@u.northwestern.edu>
2021-12-13 11:31:38 -0600

    auth fix in frontend

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -224,7 +224,7 @@ type Query {
 }
 
 type Mutation {
-    addUser(username: String!, email: String!, password: String!, isAdmin: Boolean!): Auth
+    addUser(username: String!, email: String!, password: String!, isAdmin: Boolean): Auth
     updateUser(username: String!, email: String!, password: String!): User
     login(email: String!, password: String!): Auth
 }
